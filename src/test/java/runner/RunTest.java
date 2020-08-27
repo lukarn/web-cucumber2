7e784d9d9834df93e621fd35dc195cff0fa65c1a
Łukasz Arndt <lukarn@gmail.com>
2020-08-27 21:38:45 +0200

    feature practises v03

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -1,15 +1,14 @@
-package Runner;
+package runner;
 
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 import org.junit.runner.RunWith;
-
+//@CucumberOptions(features="features",glue={"stepDefinitions"},plugin = { "pretty" }, monochrome = true)
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="features",glue={"stepDefinitions"},plugin = { "pretty" },
-        monochrome = true)
+@CucumberOptions(features="features",glue={"stepDefinitions"},plugin = { "pretty", "html:target/html-cucumber.html" }, monochrome = true)
 public class RunTest
 {
 
